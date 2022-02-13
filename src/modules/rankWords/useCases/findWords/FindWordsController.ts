8e407b341b66f2b0ef97dea930b919810cc8b320
wueliton <paulo.wueliton@hotmail.com>
2022-02-13 14:11:34 -0300

    Correção no endpoint de busca

--- src/modules/rankWords/useCases/findWords/FindWordsController.ts
+++ src/modules/rankWords/useCases/findWords/FindWordsController.ts
@@ -2,30 +2,28 @@ import { Request, Response } from "express";
 import { FindWordsUseCase } from "./FindWordsUseCase";
 
 export class FindWordsController {
-  handle() {
-    return (req: Request, res: Response) => {
-      if (req.app.get("runing"))
-        return res.status(409).json({
-          error: "Busca em progresso",
-          message:
-            "Uma busca já está sendo realizada. Aguarde para que possa adicionar uma nova.",
-        });
-
-      const { url, keywords, client } = req.body;
-
-      if (!url || !keywords || !Array.isArray(keywords) || !client)
-        return res.status(400).json({
-          message:
-            "Requisição mal formatada, verifique os dados e tente novamente.",
-        });
-
-      const findWordsUseCase = new FindWordsUseCase(req, client, url, keywords);
-      req.app.set("runing", true);
-      findWordsUseCase.execute();
-
-      res.json({
-        message: "Realizando busca",
+  handle(req: Request, res: Response) {
+    if (req.app.get("runing"))
+      return res.status(409).json({
+        error: "Busca em progresso",
+        message:
+          "Uma busca já está sendo realizada. Aguarde para que possa adicionar uma nova.",
       });
-    };
+
+    const { url, keywords, client } = req.body;
+
+    if (!url || !keywords || !Array.isArray(keywords) || !client)
+      return res.status(400).json({
+        message:
+          "Requisição mal formatada, verifique os dados e tente novamente.",
+      });
+
+    const findWordsUseCase = new FindWordsUseCase(req, client, url, keywords);
+    req.app.set("runing", true);
+    findWordsUseCase.execute();
+
+    res.json({
+      message: "Realizando busca",
+    });
   }
 }
