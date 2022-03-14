e80fbe08e21b0bbfe28bfeaa4e10819411804e0a
wueliton <paulo.wueliton@hotmail.com>
2022-03-14 10:59:24 -0300

    Removido erro 429

--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -12,5 +12,5 @@ export const ApiService = axios.create({
       "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.106 Safari/537.36 OPR/38.0.2220.41",
   },
   responseType: "arraybuffer",
-  validateStatus: () => false,
+  validateStatus: () => true,
 });
